7c3331f0f504ce7b98df8b990d7fd3a08ea62a41
JacobCrown <jkorona.pl@gmail.com>
2023-05-18 21:28:39 +0200

    add constants for path management, minor code refactoring

---
+++ code/common/constants.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+PROJECT_DIRPATH = Path(str(__file__)).parent.parent.parent
+
+CODE_DIRPATH = PROJECT_DIRPATH / 'code'
--- code/tile.py
+++ code/tile.py
@@ -1,8 +1,11 @@
 import pygame
 from settings import *
 
+import common.constants as c
+
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
+        image_path = c.PROJECT_DIRPATH / 'graphics/test/tile.png'
+        self.image = pygame.image.load(image_path).convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)

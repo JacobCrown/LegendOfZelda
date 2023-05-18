dec31a4d81f71228ed46e7f8e51c4de14f9b565d
JacobCrown <jkorona.pl@gmail.com>
2023-05-18 21:15:38 +0200

    reorganize project

---
+++ code/main.py
@@ -0,0 +1,30 @@
+import pygame
+import sys
+from settings import *
+
+from level import Level
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('Legend Of Zelda')
+
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()

35c20704e9c5084ba3655306b7e1ed49b491f428
JacobCrown <jkorona.pl@gmail.com>
2023-05-19 18:22:10 +0200

    add displaying sprites

--- code/level.py
+++ code/level.py
@@ -1,5 +1,9 @@
 import pygame
 
+from tile import Tile
+from settings import *
+from player import Player
+
 class Level:
     def __init__(self):
         # get the display surface
@@ -7,5 +11,19 @@ class Level:
         self.visible_sprites = pygame.sprite.Group()
         self.obstacle_sprites = pygame.sprite.Group()
 
+        # sprite setup
+        self.create_map()
+
+    def create_map(self):
+        for row_idx, row in enumerate(WORLD_MAP):
+            for col_idx, col in enumerate(row):
+                x = col_idx * TILESIZE
+                y = row_idx * TILESIZE
+                if col == "x":
+                    Tile((x,y), [self.visible_sprites])
+                elif col == "p":
+                    Tile((x,y), [self.visible_sprites])
+
+                    
     def run(self):
-        pass
+        self.visible_sprites.draw(self.display_surface)
--- code/tile.py
+++ code/tile.py
@@ -6,6 +6,6 @@ import common.constants as c
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        image_path = c.PROJECT_DIRPATH / 'graphics/test/tile.png'
+        image_path = c.PROJECT_DIRPATH / 'graphics/test/rock.png'
         self.image = pygame.image.load(image_path).convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)

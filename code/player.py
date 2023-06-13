d30b50e52853800c65293bda31dfd5af118b1aa4
JacobCrown <jkorona.pl@gmail.com>
2023-06-13 23:31:38 +0200

    add possibility to change weapons

--- code/player.py
+++ code/player.py
@@ -34,8 +34,11 @@ class Player(pygame.sprite.Sprite):
         # weapon
         self.create_attack = create_attack
         self.destroy_attack = destroy_attack
-        self.weapon_index = 0
+        self.weapon_index = 3
         self.weapon = list(weapon_data.keys())[self.weapon_index]
+        self.can_switch_weapon = True
+        self.weapon_switch_time = None
+        self.switch_duration_cooldown = 200
 
 
     def import_player_assets(self):
@@ -85,6 +88,12 @@ class Player(pygame.sprite.Sprite):
             if keys[pygame.K_LCTRL]:
                 self.attacking = True
                 self.attack_time = pygame.time.get_ticks()
+
+            if keys[pygame.K_q] and self.can_switch_weapon:
+                self.can_switch_weapon = False
+                self.weapon_switch_time = pygame.time.get_ticks()
+                self.weapon_index += 1
+                self.weapon = list(weapon_data.keys())[self.weapon_index % 5]
 
     def get_status(self):
         # idle status
@@ -142,6 +151,10 @@ class Player(pygame.sprite.Sprite):
             if current_time - self.attack_time >= self.attack_cooldown:
                 self.attacking = False
                 self.destroy_attack()
+
+        if not self.can_switch_weapon:
+            if current_time - self.weapon_switch_time >= self.switch_duration_cooldown:
+                self.can_switch_weapon = True
 
     def animate(self):
         self.status = self.get_status()

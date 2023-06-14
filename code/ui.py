f5e72832a8965afa5ebb6b499f6ea8dfa6d0f517
JacobCrown <jkorona.pl@gmail.com>
2023-06-14 20:59:25 +0200

    improve showing health and energy levels

--- code/ui.py
+++ code/ui.py
@@ -13,6 +13,24 @@ class UI:
         self.health_bar_rect = pygame.Rect(10, 10, HEALTH_BAR_WIDTH, BAR_HEIGHT)
         self.energy_bar_rect = pygame.Rect(10, 34, ENERGY_BAR_WIDTH, BAR_HEIGHT)
 
+    def show_bar(self, current_amount, max_amount, bg_rect: pygame.Rect, color):
+        # draw bg
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
+
+        # converting stat to pixel 
+        ratio: float = current_amount / max_amount
+        current_width = int(bg_rect.width * ratio)
+        current_rect = bg_rect.copy()
+        current_rect.width = current_width
+
+        # drawing the bar
+        pygame.draw.rect(self.display_surface, color, current_rect)
+
+
+
     def display(self, player: Player):
-        pygame.draw.rect(surface=self.display_surface, color='black',
-                         rect=self.health_bar_rect)
+        self.show_bar(player.health, player.stats['health'], self.health_bar_rect,
+                      HEALTH_COLOR)
+        self.show_bar(player.energy, player.stats['energy'], self.energy_bar_rect,
+                      ENERGY_COLOR)
+
